eaa5e50dc0bd1f436aa49f2a37e0a5051830af2d
V. Glenn Tarcea <gtarcea@umich.edu>
2013-12-02 13:38:43 -0500

    Cleanup Exists. Add Copy to copy a file.

--- handyfile/file.go
+++ handyfile/file.go
@@ -4,6 +4,7 @@ import (
 	"hash/crc32"
 	"io/ioutil"
 	"os"
+	"io"
 )
 
 func Checksum32(path string) uint32 {
@@ -16,9 +17,26 @@ func Checksum32(path string) uint32 {
 }
 
 func Exists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
+}
+
+func Copy(src, dest string) error {
+	fsrc, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer fsrc.Close()
+	
+	fdest, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer fdest.Close()
+
+	if _, err := io.Copy(fdest, fsrc); err != nil {
+		return err
 	}
 
-	return true
+	return nil
 }
