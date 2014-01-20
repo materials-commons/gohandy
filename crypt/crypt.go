b35063c96a5a9510d857b2a09854fd863dfb8335
V. Glenn Tarcea <gtarcea@umich.edu>
2014-01-20 17:08:56 -0500

    Interface to unix crypt function.

---
+++ crypt/crypt.go
@@ -0,0 +1,22 @@
+package crypt
+
+import(
+	"unsafe"
+)
+
+// #cgo LDFLAGS: -lcrypt
+// #define _GNU_SOURCE
+// #include <crypt.h>
+// #include <stdlib.h>
+import "C"
+
+// Crypt wraps C library crypt_r function
+func Crypt(key, salt string) string {
+	data := C.struct_crypt_data{}
+	ckey := C.CString(key)
+	csalt := C.CString(salt)
+	out := C.GoString(C.crypt_r(ckey, csalt, &data))
+	C.free(unsafe.Pointer(ckey))
+	C.free(unsafe.Pointer(csalt))
+	return out
+}
