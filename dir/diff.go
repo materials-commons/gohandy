7f47680f1e8ede0974be40f6d0abd435adef37dd
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-25 17:05:26 -0400

    Additional updates as I work through the sync algorithm.

--- dir/diff.go
+++ dir/diff.go
@@ -7,22 +7,36 @@ func Diff(originalVersion *Directory, newerVersion *Directory) []*Patch {
 	newFiles := newerVersion.Flatten()
 	originalFilesLen, newFilesLen := len(originalFiles), len(newFiles)
 	originalFilesIndex, newFilesIndex := 0, 0
+	patches := []*Patch{}
+
+	// The checks need to become more complex. In order to determine if we should
+	// add or delete a file, we need to look at a couple of things:
+	// 1. Who has the latest set of updates? We assume here that newer wins.
+	// 2. What are the events if we are monitoring events. Because the events can
+	//    tell us what really happened.
+	// 3.
 
 DIR_COMPARE_LOOP:
 	for {
 		switch {
 		case originalFilesIndex >= originalFilesLen && newFilesIndex >= newFilesLen:
 			break DIR_COMPARE_LOOP
-		case originalFilesIndex >= originalFilesLen:
-			// We are at the end of the list for originalFiles any files in originalFiles are
-			// not in newFiles. We treat this as a delete from originalFiles.
 
-			// do something
+		case originalFilesIndex >= originalFilesLen:
+			// We are at the end of the list for originalFiles any files in newFiles are
+			// not in originalFiles. We treat as a creation, and add the entries from newFiles
+			// to originalFiles
+			patch := &Patch{
+				File:    newFiles[newFilesIndex],
+				Type:    PatchCreate,
+				ApplyTo: OriginalDirectory,
+			}
+			patches = append(patches, patch)
+
 		case newFilesIndex >= newFilesLen:
 			// We are at the end of the list for newFiles. Any files in originalFiles were added
-			// and are not in newFiles. We treat this as a add.
+			// and are not in newFiles. We treat this as an add.
 
-			// do something
 		case originalFiles[originalFilesIndex].Path > newFiles[newFilesIndex].Path:
 			// There is a file in originalFiles that is not in newFiles - add it
 
@@ -31,14 +45,17 @@ DIR_COMPARE_LOOP:
 			// entry again against another entry in newFiles. We will keep doing this until
 			// newFiles catches up or we run out of newFiles entries.
 			originalFilesIndex--
+
 		case originalFiles[originalFilesIndex].Path < newFiles[newFilesIndex].Path:
 			// There is a file in newFiles that is not in originalFiles - delete it
+			// from newFiles.
 
 			// Decrement newFilesIndex because we are going to increment at the bottom
 			// of the loop. Thus this decrement means we will be comparing this same newFile
 			// entry again against another entry in originalFiles. We will keep doing this until
 			// originalFiles catches up or we run out of originalFiles entries.
 			newFilesIndex--
+
 		default:
 			// The names would match - check stats to determine if there is a change.
 			// if original is newer than newer then we need to update the new file. Else if
--- dir/patch.go
+++ dir/patch.go
@@ -11,9 +11,21 @@ const (
 	PatchDelete
 )
 
-// Patch is an instance of a difference when comparing two directories.
+// PathcAppliedTo tells us which directory set to apply the patch to.
+type PatchApplyTo int
+
+const (
+	// OriginalDirectory apply the patch to the original directory set.
+	OriginalDirectory PatchApplyTo = iota
+
+	// NewDirectory apply the patch to the new directory set.
+	NewDirectory
+)
+
+// Patch is an instance of a difference when comparing two directories. It specifies
+// the kind of change to apply and where to apply it to.
 type Patch struct {
-	Path string
-	File *File
-	Type PatchType
+	File    *File
+	Type    PatchType
+	ApplyTo PatchApplyTo
 }
