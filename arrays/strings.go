2d5303d4031281c215b115d7609fa087912833a8
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-28 17:07:29 -0400

    Add Remove and refactor to allow for multiple types and easy access.

--- arrays/strings.go
+++ arrays/strings.go
@@ -1,10 +1,13 @@
 package arrays
 
 // Strings allows us to limit our filter to just arrays of strings
-type Strings struct{}
+type strings struct{}
+
+// Strings allows easy access to the functions that operate on a list of strings
+var Strings strings
 
 // Filter filters an array of strings.
-func (s Strings) Filter(in []string, keep func(item string) bool) []string {
+func (s strings) Filter(in []string, keep func(item string) bool) []string {
 	var out []string
 	for _, item := range in {
 		if keep(item) {
@@ -13,6 +16,19 @@ func (s Strings) Filter(in []string, keep func(item string) bool) []string {
 	}
 
 	return out
+}
+
+// Remove filters an array by removing all matching items
+func (s strings) Remove(in []string, remove ...string) []string {
+	return s.Filter(in, func(item string) bool {
+		found := false
+		for _, removeItem := range remove {
+			if removeItem == item {
+				found = true
+			}
+		}
+		return !found
+	})
 }
 
 
