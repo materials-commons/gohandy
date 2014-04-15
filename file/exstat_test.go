f41c36143a542e83649af19e596c72ab96184302
V. Glenn Tarcea <gtarcea@umich.edu>
2014-04-15 16:46:56 -0400

    Add a very cheap unit test for ExStat.

---
+++ file/exstat_test.go
@@ -0,0 +1,20 @@
+package file
+
+import (
+	"fmt"
+	"testing"
+)
+
+var _ = fmt.Println
+
+func TestExFileInfo(t *testing.T) {
+	fi, err := ExStat("exstat.go")
+	if err != nil {
+		t.Fatalf("ExStat failed: %s", err)
+	}
+
+	// Cheap test - just look at the values
+	fmt.Println("ctime", fi.CTime())
+	fmt.Println("atime", fi.ATime())
+	fmt.Println("inode", fi.INode())
+}
