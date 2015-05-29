8a5f887b6fcad70e110851589098ac4efc3dbac9
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-29 12:23:50 -0400

    Add interface to os file operations.

---
+++ file/operations.go
@@ -0,0 +1,39 @@
+package file
+
+import "os"
+
+// Operations is the operations on a file. The interface
+// exists to allow mocking of file system operations.
+type Operations interface {
+	Remove(path string) error
+	RemoveAll(path string) error
+	Mkdir(path string, perm os.FileMode) error
+	MkdirAll(path string, perm os.FileMode) error
+}
+
+// osOperations implements the os package operations
+type osOperations struct{}
+
+// OS allows access to the os package methods that are the
+// same as in the Operations interface.
+var OS osOperations
+
+// Remove is a wrapper around os.Remove
+func (_ osOperations) Remove(path string) error {
+	return os.Remove(path)
+}
+
+// RemoveAll is a wrapper around os.RemoveAll
+func (_ osOperations) RemoveAll(path string) error {
+	return os.RemoveAll(path)
+}
+
+// Mkdir is a wrapper around os.Mkdir
+func (_ osOperations) Mkdir(path string, perm os.FileMode) error {
+	return os.Mkdir(path, perm)
+}
+
+// MkdirAll is a wrapper around os.MkdirAll
+func (_ osOperations) MkdirAll(path string, perm os.FileMode) error {
+	return os.MkdirAll(path, perm)
+}
