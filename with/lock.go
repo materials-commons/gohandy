0ef909093cf4b665653ee1d65a43271f01c314b5
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-25 11:01:31 -0400

    Add new package with for calling in a certain context. Added lock for calling func with a lock held.

---
+++ with/lock.go
@@ -0,0 +1,62 @@
+package with
+
+import "sync"
+
+// ReadLock calls fn after acquiring a read lock.
+func ReadLock(mutex *sync.RWMutex, fn func()) {
+	defer mutex.RUnlock()
+	mutex.RLock()
+	fn()
+}
+
+// WriteLock calls fn after acquiring a write lock.
+func WriteLock(mutex *sync.RWMutex, fn func()) {
+	defer mutex.Unlock()
+	mutex.Lock()
+	fn()
+}
+
+// An RWLocker is an interface that acquires
+// a lock before calling the given func.
+type RWLocker interface {
+	WithReadLock(fn func())
+	WithWriteLock(fn func())
+}
+
+// RWLock implements the RWLocker interface. It takes a function
+// that wraps the function to call. This allows the user
+// to perform common book keeping or checks before calling
+// the function passed to WithReadLock or WithWriteLock.
+type RWLock struct {
+	mutex *sync.RWMutex
+	fn    func(fn func())
+}
+
+func NewRWLock(fn func()) *RWLock {
+	fnToCall := fn
+	if fnToCall == nil {
+		fnToCall = func(fun func()) {
+			fun()
+		}
+	}
+	return &RWLock{
+		mutex: &sync.RWMutex{},
+		fn:    fnToCall,
+	}
+}
+
+// WithReadLock acquires a read lock and then calls
+// the RWLock function passing it the fn.
+func (lock *RWLock) WithReadLock(fn func()) {
+	defer lock.mutex.RUnlock()
+	lock.mutex.RLock()
+	lock.fn(fn)
+}
+
+// WithWriteLock acquires a write lock and then calls
+// the RWLock function passing it the fn.
+func (lock *RWLock) WithWriteLock(fn func()) {
+	defer lock.mutex.Unlock()
+	lock.mutex.Lock()
+	lock.fn(fn)
+}
